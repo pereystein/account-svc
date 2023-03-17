39205b8c595bc33d6af23b87f9e4b7690ceb1a82
per.eystein.saebo <per.eystein.saebo@baxbid.no>
2023-03-17 20:40:13 +0100

    initialize project

---
+++ src/test/java/org/pes/account/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pes.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

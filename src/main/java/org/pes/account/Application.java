e448719b3f09107f3c231dfaa6cc11a580b3a0ea
per.eystein.saebo <per.eystein.saebo@baxbid.no>
2023-03-17 20:53:45 +0100

    set up swagger

--- src/main/java/org/pes/account/Application.java
+++ src/main/java/org/pes/account/Application.java
@@ -2,12 +2,14 @@ package org.pes.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-public class AccountSvcApplication {
+@EnableSwagger2
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AccountSvcApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
